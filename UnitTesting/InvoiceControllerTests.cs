99cb1ffacf6f8b46f12b35e0b5e7d8b36d0014c9
Kaumadi Marasinghe <7821403+kaumadi@users.noreply.github.com>
2023-02-13 08:57:50 +0530

    Update InvoiceControllerTests.cs
    Add unit Test

--- UnitTesting/InvoiceControllerTests.cs
+++ UnitTesting/InvoiceControllerTests.cs
@@ -10,34 +10,7 @@ namespace UnitTesting
 {
     public class InvoiceControllerTests
     {
-        //[Fact]
-        //public async void Task_Add_ValidData_Return_OkResult()
-        //{
-        //    //Arrange
-        //    var controller = new PostController(repository);
-        //    var post = new Post() { Title = "Test Title 3", Description = "Test Description 3", CategoryId = 2, CreatedDate = DateTime.Now };
-
-        //    //Act
-        //    var data = await controller.AddPost(post);
-
-        //    //Assert
-        //    Assert.IsType<OkObjectResult>(data);
-        //}
-
-        //[Fact]
-        //public async void Task_Add_InvalidData_Return_BadRequest()
-        //{
-        //    //Arrange
-        //    var controller = new PostController(repository);
-        //    Post post = new Post() { Title = "Test Title More Than 20 Characteres", Description = "Test Description 3", CategoryId = 3, CreatedDate = DateTime.Now };
-
-        //    //Act            
-        //    var data = await controller.AddPost(post);
-
-        //    //Assert
-        //    Assert.IsType<BadRequestResult>(data);
-        //}
-
+      
         [Fact]
         public void Test_POST_AddReservation()
         {
@@ -84,6 +57,27 @@ namespace UnitTesting
             // Assert
             var reservation = Assert.IsType<string>(result);
             Assert.IsType<OkObjectResult>(result);
+        }
+        
+        [Fact]
+        public async void Test_Add_InvalidData_Return_BadRequest()
+        {
+            InvoiceDTO inv = new InvoiceDTO()
+            {
+                Date = DateTime.Now,
+                TotalAmount = 1000
+            };
+            var mockService = new Mock<IInvoiceService>();
+            var logService = new Mock<ILogger<InvoicesController>>();
+            mockService.Setup(t => t.AddInvoiceAsync(It.IsAny<InvoiceDTO>())).ReturnsAsync("");
+            var controller = new InvoicesController(mockService.Object, logService.Object);
+
+            // Act
+            var result = controller.AddInvoice(inv);
+
+            // Assert
+            var invoice = Assert.IsType<Invoice>(result);
+            Assert.IsType<BadRequestResult>(result);
         }
 
     }
